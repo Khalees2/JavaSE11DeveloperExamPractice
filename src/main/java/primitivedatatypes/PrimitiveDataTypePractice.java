55403db0701cf8e65c5126e8f6398a283404b9c0
khalees2 <salmank640@gmail.com>
2024-05-26 09:57:33 +0100

    Declared primitive data types

---
+++ src/main/java/primitivedatatypes/PrimitiveDataTypePractice.java
@@ -0,0 +1,17 @@
+package primitivedatatypes;
+
+public class PrimitiveDataTypePractice {
+    public static void main(String[] args) {
+
+        //Declaration of local primitive data types
+        byte myByte;
+        short myShort;
+        int myInt;
+        //We can declare two variables of same data type in a single line.
+        float myFloatOne, myFloatTwo;
+        long myLong;
+        char myChar;
+        double myDouble;
+        boolean myBoolean;
+    }
+}
