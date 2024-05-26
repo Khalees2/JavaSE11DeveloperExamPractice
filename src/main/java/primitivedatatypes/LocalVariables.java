53cb9968b0ad3ebdea91e85e6491bae6db6d9261
khalees2 <salmank640@gmail.com>
2024-05-26 10:33:18 +0100

    Default value demo

---
+++ src/main/java/primitivedatatypes/LocalVariables.java
@@ -0,0 +1,10 @@
+package primitivedatatypes;
+
+public class LocalVariables {
+    static int myIntStatic;
+    int classInt;
+    public static void main(String[] args) {
+        System.out.println(myIntStatic);
+        System.out.println(new LocalVariables().classInt);
+    }
+}
