768dffb02ed0abd78bfca11328514f396cfc2655
khalees2 <salmank640@gmail.com>
2024-05-26 10:37:35 +0100

    Local variables must be initialised

--- src/main/java/primitivedatatypes/LocalVariables.java
+++ src/main/java/primitivedatatypes/LocalVariables.java
@@ -6,5 +6,12 @@ public class LocalVariables {
     public static void main(String[] args) {
         System.out.println(myIntStatic);
         System.out.println(new LocalVariables().classInt);
+
+        int localVariable;
+        //System.out.println(localVariable); // We must initialize
+        localVariable=127;
+        System.out.println(localVariable);
+
+        //It's okay to have uninitialized variables at class level/static variables but local variables must be initialised.
     }
 }
